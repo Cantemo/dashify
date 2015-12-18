8763ef7673fb2d05462f3b685c81945078873fea
Mattias Amnefelt <mattiasa@cantemo.com>
2015-12-18 20:08:31 +0100

    Set pkgconfig path to our installed ffmpeg

--- pkg/jenkins/build.sh
+++ pkg/jenkins/build.sh
@@ -1,9 +1,16 @@
+#! /bin/bash
+
 cd $WORKSPACE/lib/ffmpeg
 
+FFMPEG_INSTALL_PATH=$WORKSPACE/ffmpeg-install/
+
 # Build with as little code as possible
-./configure --disable-encoders --disable-decoders --disable-hwaccels --disable-bsfs --disable-devices --disable-filters --disable-swresample --disable-swscale --prefix=$WORKSPACE/ffmpeg-install/ --disable-decoder=h264 --disable-decoder=h263
+./configure --disable-encoders --disable-decoders --disable-hwaccels --disable-bsfs --disable-devices --disable-filters --disable-swresample --disable-swscale --prefix=$FFMPEG_INSTALL_PATH --disable-decoder=h264 --disable-decoder=h263
 make
 make install
+
+PKG_CONFIG_PATH=$FFMPEG_INSTALL_PATH/lib/pkgconfig/
+export PKG_CONFIG_PATH
 
 cd $WORKSPACE/src
 make
